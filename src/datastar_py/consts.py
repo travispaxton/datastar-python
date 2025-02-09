5760ccc6edc3e18cdaa30f48798b2fe4e6027928
Ben Croker <ben@putyourlightson.com>
2025-02-08 19:33:26 -0600

    Revert "Squashed commit of the following:"
    This reverts commit c6794bd1e987f3e9613342c316786f1256030ccc.

--- src/datastar_py/consts.py
+++ src/datastar_py/consts.py
@@ -51,7 +51,7 @@ class EventType(StrEnum):
 #endregion Enums
 
 DATASTAR_KEY = "datastar"
-VERSION                   = "1.0.0-beta.4"
+VERSION                   = "1.0.0-beta.3"
 
 #region Default durations
 
